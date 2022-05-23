41b4c53b28ec5cb063006b7a7b15c720f482a839
Omartinezangulo <103291599+Omartinezangulo@users.noreply.github.com>
2022-05-22 21:34:04 -0700

    pull

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -4,13 +4,6 @@ import org.junit.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-
-
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.List;
 
 public class MarkdownParseTest {
@@ -70,7 +63,8 @@ public class MarkdownParseTest {
         List<String> expect = List.of("https://something.com", "some-page.html");
         assertEquals(MarkdownParse.getLinks(contents), expect);
     }
- 
+    
+
     @Test
     public void snippet1() throws IOException {
 
